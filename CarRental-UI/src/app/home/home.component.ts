80980df53423d532fbdcebc396a064ed4b052bd4
Natyra Elshani <natyra.elshani@atis.al>
2019-08-30 17:00:12 +0200

    ...

--- CarRental-UI/src/app/home/home.component.ts
+++ CarRental-UI/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationService } from '../_services/location.service';
+import { Location } from '../_models/Location';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+locations: Location[];
+  constructor(private locationService: LocationService) { }
 
   ngOnInit() {
+    this.locationsList();
+  }
+
+  locationsList() {
+    this.locationService.getLocations().subscribe((locations: Location[]) => {
+this.locations = locations;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
